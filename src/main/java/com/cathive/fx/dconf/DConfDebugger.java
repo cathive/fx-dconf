1be7c1bf1f75d999fcc0718e8aada5bd588ad056
Benjamin P. Jung <headcr4sh@gmail.com>
2013-08-05 16:21:49 +0200

    Enhance mapping of C-API
    Add ability to read and write booleans, bytes and strings.

--- src/main/java/com/cathive/fx/dconf/DConfDebugger.java
+++ src/main/java/com/cathive/fx/dconf/DConfDebugger.java
@@ -3,6 +3,8 @@ package com.cathive.fx.dconf;
 import org.gnome.dconf.Client;
 import org.gnome.dconf.DConf;
 
+import java.util.List;
+
 /**
  * @author Benjamin P. Jung
  */
@@ -10,7 +12,21 @@ public class DConfDebugger {
 
     public static void main(String[] args) {
 
+        System.out.println("Creating DConf client...");
         final Client client = Client.create();
+
+        client.writeBoolean("/com/example/ExampleApp/exampleBoolean", false);
+        client.writeByte("/com/example/ExampleApp/exampleByte", (byte) 1);
+        client.writeString("/com/example/ExampleApp/exampleString", "Hello, World!");
+
+        System.out.println("/some/bogus/Stuff/key = " + client.readBoolean("/some/bogus/Stuff/key"));
+        System.out.println("/org/gnome/Contacts/did-initial-setup = " + client.readBoolean("/org/gnome/Contacts/did-initial-setup"));
+        System.out.println("/org/gnome/Contacts/view-subset = " + client.readString("/org/gnome/Contacts/view-subset"));
+
+        final List<String> apps = client.list("org/gnome/desktop");
+        for (final String app: apps) {
+            System.out.println(app);
+        }
 
     }
 
--- src/main/java/org/gnome/dconf/DConf.java
+++ src/main/java/org/gnome/dconf/DConf.java
@@ -6,29 +6,54 @@ import org.bridj.Pointer;
 import org.bridj.StructObject;
 import org.bridj.ann.*;
 import org.bridj.ann.Runtime;
+import org.gtk.glib.GLib.gboolean;
+import org.gtk.glib.GLib.gchar;
+import org.gtk.glib.GLib.gint;
+import org.gtk.glib.GLib.GError;
+import org.gtk.glib.GLib.GVariant;
 
 /**
  * @author Benjamin P. Jung
  */
 @Library("dconf")
 @Runtime(CRuntime.class)
-public class DConf {
+public final class DConf {
 
     static {
         BridJ.register(DConf.class);
     }
 
-    @Name("dconf_client_new")
-    public static native Pointer<Client> clientNew();
+    private DConf() {}
 
+    @Name("dconf_client_new")
+    public static native Pointer<DConfClient> clientNew();
+
+    @Name("dconf_client_read")
+    public static native Pointer<GVariant> clientRead(Pointer<DConfClient> client,
+                                                      @gchar Pointer<Byte> key);
+
+    @Name("dconf_client_list")
+    public static native @gchar Pointer<Pointer<Byte>> clientList(Pointer<DConfClient> client,
+                                                                  @gchar Pointer<Byte> dir,
+                                                                  @gint Pointer<Integer> length);
+
+    @Name("dconf_client_is_writable")
+    public static native @gboolean boolean clientIsWritable(Pointer<DConfClient> client,
+                                                            @gchar Pointer<Byte> key);
+
+    @Name("dconf_client_write_fast")
+    public static native @gboolean boolean clientWriteFast(Pointer<DConfClient> client,
+                                                           @gchar Pointer<Byte> key,
+                                                           Pointer<GVariant> value,
+                                                           Pointer<Pointer<GError>> error);
+
+
+    @Struct
     @Name("DConfClient")
-    public static final class Client extends StructObject {
-        public Client() {
-            super();
-        }
-        public Client(Pointer<Client> peer) {
-            super(peer);
-        }
+    public static final class DConfClient extends StructObject {
+        public DConfClient() { super(); }
+        public DConfClient(Pointer<DConfClient> peer) { super(peer); }
+        public Pointer<DConfClient> getPeer() { return (Pointer<DConfClient>) this.peer; }
     }
 
 }
